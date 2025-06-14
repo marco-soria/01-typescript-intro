3aedd0cc8710511c8b6e3019d91689eebe58fabc
marco-soria <marco.soriaa7@gmail.com>
2025-06-14 09:02:33 -0500

    basic types

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,7 @@
+const name: string = "strider";
+let hpPoints: number | "Full" = 95;
+const isAlive: boolean = true;
+
+hpPoints = "Full";
+
+console.log(`Name: ${name}, HP: ${hpPoints}, Alive: ${isAlive}`);
